6bff0518870d1bbaee39db1348a645a6438f0656
TsT <tst2005@gmail.com>
2016-10-14 13:31:12 +0200

    cleanup

--- gen.sh
+++ gen.sh
@@ -2,5 +2,4 @@
 
 cd -- "$(dirname "$0")"
 
-#cat luaver.d/01common.header luaver.d/02lua luaver.d/03luajit luaver.d/04luarocks luaver.d/05common.footer > luaver.new
 cat luaver.d/01common.header luaver.d/02lua.d/* luaver.d/03luajit.d/* luaver.d/04luarocks.d/* luaver.d/05common.footer > luaver.new
