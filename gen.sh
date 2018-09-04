4c9351eec1437e90d1d1e2e24e7c87b68646a7c3
TsT <tst2005@gmail.com>
2018-09-04 14:32:24 +0200

    generator updated

--- gen.sh
+++ gen.sh
@@ -6,5 +6,5 @@ cd -- "$(dirname "$0")"
 for f in $(find luaver.d/ -type f -name '*.sh' | sort ); do
 	cat "$f"
 done
-) > luaver.new2
-#cat luaver.d/01common.header luaver.d/01generic.d/* luaver.d/02lua.d/* luaver.d/03luajit.d/* luaver.d/04luarocks.d/* luaver.d/05common.footer > luaver.new
+) > luaver.new
+chmod +x luaver.new
