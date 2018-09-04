7dd8368a55f8a7f4d3a5b4007d2a963716008b96
TsT <tst2005@gmail.com>
2018-09-04 13:56:02 +0200

    rename generic.d to 01generic.d

--- gen.sh
+++ gen.sh
@@ -2,4 +2,4 @@
 
 cd -- "$(dirname "$0")"
 
-cat luaver.d/01common.header luaver.d/generic.d/* luaver.d/02lua.d/* luaver.d/03luajit.d/* luaver.d/04luarocks.d/* luaver.d/05common.footer > luaver.new
+cat luaver.d/01common.header luaver.d/01generic.d/* luaver.d/02lua.d/* luaver.d/03luajit.d/* luaver.d/04luarocks.d/* luaver.d/05common.footer > luaver.new
