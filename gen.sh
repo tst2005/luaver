7eb0ec372156f9b23474c85645882cbf70137cc2
TsT <tst2005@gmail.com>
2018-09-04 14:07:35 +0200

    rename all partial files with .sh extension

--- gen.sh
+++ gen.sh
@@ -2,4 +2,9 @@
 
 cd -- "$(dirname "$0")"
 
-cat luaver.d/01common.header luaver.d/01generic.d/* luaver.d/02lua.d/* luaver.d/03luajit.d/* luaver.d/04luarocks.d/* luaver.d/05common.footer > luaver.new
+(
+for f in $(find luaver.d/ -type f | sort ); do
+	cat "$f"
+done
+) > luaver.new2
+#cat luaver.d/01common.header luaver.d/01generic.d/* luaver.d/02lua.d/* luaver.d/03luajit.d/* luaver.d/04luarocks.d/* luaver.d/05common.footer > luaver.new
